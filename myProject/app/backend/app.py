b82cfd30ffc065bbc28fcf066c5cb8b17a885019
Rayiemas M Putra <rayiemasmputra@Rayiemass-MacBook-Pro.local>
2023-04-15 11:21:06 +0700

    last commit

--- myProject/app/backend/app.py
+++ myProject/app/backend/app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template, request, jsonify
-
-import pymongo
+from flask import Flask, jsonify, request
+from flask_opentracing import FlaskTracer
 from flask_pymongo import PyMongo
+from jaeger_client import Config
+from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+
+import json
+import logging
+import opentracing
+
+
+def initialize_tracer():
+    """
+    Initializes an instance of the Jaeger tracer.
+    """
+
+    logging.getLogger("").handlers = []
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+    open_tracing_config = {"sampler": {"type": "const", "param": 1}, "logging": True}
+    tracer_config = Config(open_tracing_config, service_name="backend")
+
+    return tracer_config.initialize_tracer()
+
 
 app = Flask(__name__)
 
@@ -9,30 +29,55 @@ app.config["MONGO_DBNAME"] = "example-mongodb"
 app.config[
     "MONGO_URI"
 ] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
-
 mongo = PyMongo(app)
+
+flask_tracer = FlaskTracer(initialize_tracer, True, app)
+flask_tracer_span = flask_tracer.get_span()
+
+metrics = GunicornInternalPrometheusMetrics(app)
 
 
 @app.route("/")
 def homepage():
-    return "Hello World"
+    with opentracing.tracer.start_span(
+        "home-endpoint", child_of=flask_tracer_span
+    ) as span:
+        response = {"message": "Home endpoint"}
+        span.set_tag("message", response)
+
+        return jsonify(response)
 
 
 @app.route("/api")
 def my_api():
-    answer = "something"
-    return jsonify(repsonse=answer)
+    with opentracing.tracer.start_span(
+        "api-endpoint", child_of=flask_tracer_span
+    ) as span:
+        response = {"message": "API endpoint"}
+        span.set_tag("message", response)
+
+        return jsonify(response)
 
 
 @app.route("/star", methods=["POST"])
 def add_star():
-    star = mongo.db.stars
-    name = request.json["name"]
-    distance = request.json["distance"]
-    star_id = star.insert({"name": name, "distance": distance})
-    new_star = star.find_one({"_id": star_id})
-    output = {"name": new_star["name"], "distance": new_star["distance"]}
-    return jsonify({"result": output})
+    with opentracing.tracer.start_span(
+        "star-endpoint", child_of=flask_tracer_span
+    ) as span:
+        try:
+            star = mongo.db.stars
+            name = request.json["name"]
+            distance = request.json["distance"]
+            star_id = star.insert({"name": name, "distance": distance})
+            new_star = star.find_one({"_id": star_id})
+            output = {"name": new_star["name"], "distance": new_star["distance"]}
+
+            response = jsonify({"result": output})
+            span.set_tag("message", json.dumps(response))
+
+            return response
+        except:
+            span.set_tag("response", "Can't retrieve output from database")
 
 
 if __name__ == "__main__":
--- myProject/app/trial/app.py
+++ myProject/app/trial/app.py
@@ -1,93 +1,58 @@
+from flask import Flask, jsonify
+from jaeger_client import Config
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+
 import logging
-import re
 import requests
 
 
-from flask import Flask, jsonify, render_template
-from flask_opentracing import FlaskTracing
-from jaeger_client import Config
-from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from prometheus_flask_exporter import PrometheusMetrics
+def initialize_tracer():
+    """
+    Initializes an instance of the Jaeger tracer.
+    """
+
+    logging.getLogger("").handlers = []
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+
+    open_tracing_config = {"sampler": {"type": "const", "param": 1}, "logging": True}
+    tracer_config = Config(open_tracing_config, service_name="trial")
+
+    return tracer_config.initialize_tracer()
 
 
 app = Flask(__name__)
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(ConsoleSpanExporter())
+)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
-
-metrics = PrometheusMetrics(app)
-# static information as metric
-metrics.info("app_info", "Application info", version="1.0.3")
-
-logging.getLogger("").handlers = []
-logging.basicConfig(format="%(message)s", level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-
-def init_tracer(service):
-
-    config = Config(
-        config={
-            "sampler": {"type": "const", "param": 1},
-            "logging": True,
-            "reporter_batch_size": 1,
-        },
-        service_name=service,
-        validate=True,
-        metrics_factory=PrometheusMetricsFactory(service_name_label=service),
-    )
-
-    # this call also sets opentracing.tracer
-    return config.initialize_tracer()
-
-
-tracer = init_tracer("trial")
-flask_tracer = FlaskTracing(tracer, True, app)
+tracer = initialize_tracer()
 
 
 @app.route("/")
 def homepage():
-    return render_template("main.html")
-
-
-@app.route("/trace")
-def trace():
-    def remove_tags(text):
-        tag = re.compile(r"<[^>]+>")
-        return tag.sub("", text)
-
     with tracer.start_span("get-python-jobs") as span:
+        homepages = []
         res = requests.get("https://jobs.github.com/positions.json?description=python")
-        span.log_kv({"event": "get jobs count", "count": len(res.json())})
-        span.set_tag("jobs-count", len(res.json()))
+        span.set_tag("first-tag", len(res.json()))
 
-        jobs_info = []
         for result in res.json():
-            jobs = {}
-            with tracer.start_span("request-site") as site_span:
-                logger.info(f"Getting website for {result['company']}")
-                try:
-                    jobs["description"] = remove_tags(result["description"])
-                    jobs["company"] = result["company"]
-                    jobs["company_url"] = result["company_url"]
-                    jobs["created_at"] = result["created_at"]
-                    jobs["how_to_apply"] = result["how_to_apply"]
-                    jobs["location"] = result["location"]
-                    jobs["title"] = result["title"]
-                    jobs["type"] = result["type"]
-                    jobs["url"] = result["url"]
+            try:
+                homepages.append(requests.get(result["company_url"]))
+            except:
+                return "Unable to get site for %s" % result["company"]
 
-                    jobs_info.append(jobs)
-                    site_span.set_tag("http.status_code", res.status_code)
-                    site_span.set_tag("company-site", result["company"])
-                except Exception:
-                    logger.error(f"Unable to get site for {result['company']}")
-                    site_span.set_tag("http.status_code", res.status_code)
-                    site_span.set_tag("company-site", result["company"])
-
-    return jsonify(jobs_info)
+    return jsonify(homepages)
 
 
 if __name__ == "__main__":
-    app.run(debug=True,)
+    app.run(debug=True)
